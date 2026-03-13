6209dd92de4d10d510e0178d9f77e438388ece06
anand raj <git add README.md>
2026-03-13 19:51:24 +0530

    project

--- script.js
+++ script.js
@@ -33,13 +33,13 @@ function swap() {
 }
 
 setInterval(swap, 30);
-let reveal = document.querySelectorAll("#skill .reveal")
+let reveal = document.querySelectorAll("#skill  .box2 .reveal")
 let container = document.querySelector("#skill .container");
 
 container.addEventListener("click", function(){
 
   reveal.forEach(function(el){
-    el.style.color = "white";
+    el.style.color = " #043742";
   })
 
 })
